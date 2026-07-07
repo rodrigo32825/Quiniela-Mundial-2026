e72c8b2b4a6c98ec836c0868b35f3c421483ea1c
rodrigo32825 <rodrigo.v.zambrano@gmail.com>
2026-07-07 12:49:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1467,9 +1467,9 @@ def render_tabla_general(data: dict):
 
 
     
-        st.dataframe(resumen, use_container_width=True, hide_index=True)
+    st.dataframe(resumen, use_container_width=True, hide_index=True)
 
-        if visible or st.session_state.is_admin:
+    if visible or st.session_state.is_admin:
         st.markdown("### Ver pronósticos de un participante")
         if participantes_list:
             participante_sel = st.selectbox("Participante", options=participantes_list)
